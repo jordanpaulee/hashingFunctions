16f143cef902d5b8cac95c0211192f0d2ee589df
Jordan Lee <jplee01@wm.edu>
2023-04-13 11:13:13 -0400

    Main implementation For Matching Output

--- hashmain.cpp
+++ hashmain.cpp
@@ -10,9 +10,14 @@ using namespace std;
 
 
 HashLin foo(0);
+HashLin test(9);
+
+
+string testArray9[9] = {"the", "quick", "brown", "fox", "jumped", "over", "a", "lazy", "dog"};
+string testArray10[10] = {"hello", "street", "leaves", "fall", "Thanksgiving", "candy", "pumpkin", "pie", "Halloween", "fun"};
 
 int main() {
-  
+  /*
     foo.insertString("the");
     foo.insertString("quick");
     foo.insertString("brown");
@@ -23,7 +28,7 @@ int main() {
     foo.insertString("lazy");
     foo.insertString("dog");
     foo.print();
-    
+    */
 /*
 hello
 street
@@ -48,6 +53,14 @@ fun
     foo.insertString("Halloween");
     foo.insertString("fun");
     foo.print();*/
+
+
+    //better implementation seems to be to read in the values into and array and then hash
+    //Matches output better for 9 and 10
+    for (int i = 0; i < 11; i++){
+        test.insertString(testArray10[i]);
+    }
+    test.print();
 }
 
 
